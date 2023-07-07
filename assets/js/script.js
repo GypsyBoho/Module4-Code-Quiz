945563241d13bfd58e73bfd8494464fe221c5f95
BohoBabe <bohobabe@BohoBabes-MBP.attlocal.net>
2023-07-06 20:20:43 -0700

    updated question arrays and orgnaized html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 // three phases 
 
-// HTML Selectors //
+// HTML Query Selectors //
 // var startButtonEL = document.querySelector("#start")
 //  var submitEl = document.querySelector('#initals-submit')
-var startButtonEL = document.querySelector("#start")
-
+var startButton = document.querySelector("#start")
+var startPage = document.querySelector("#start-page")
+var questionsPage = document.querySelector("#questions-page")
+var questionElement = document.getElementById("questions")
+var timeEl = document.querySelector("#time")
+var countdownEl = document.getElementById("countdown")
 
 // Global variables //
 // question number - current question
@@ -12,21 +16,14 @@ var currentQuestionIndex = 0;
 var score = 0;
 
 // store time - var timeLeft = 300
-var timeEl = document.querySelector(".time")
-
-var countdownEl = document.getElementById("countdown")
-
 var timeLeft = 60;
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secodnsLeft;
-    }, 1000);
-}
+var listOfHighScores = []
+var timerInterval;
+
 
 // this will show the last input in the form //
-renderLastregistered();
+// renderLastregistered();
 
 // to store more than one set of values in the local storage
 // have an array
@@ -35,32 +32,69 @@ renderLastregistered();
 // to get the value back --
 // get array from storage - JASON.parse()
 
-// when the submit button is clicked to submit the final score and initals // 
-sumbitButton.addEventListner("click", function(event))
+
 
 // to keep the list of names and scores have an empty var that passes through the key and value pair for each name and score
-var listOfHighScores[]
 
-var questionElement = document.getElementById("questions")
 
 // array/pool of questions - var currentQuestions = ["arry of questions",]
+var questions = [
+    {
+        question: "What is the process by which plants convert light into energy?",
+        choices: ["Photosynthesis", "Respiration", "Germination"],
+        correctAnswers: "Photosynthesis"
+
+    },
+    {
+        question: "What is the process by which plants convert light into energy?",
+        choices: ["Photosynthesis", "Respiration", "Germination"],
+        correctAnswers: "Photosynthesis"
+    },
+    {
+        question: "What is the process by which plants convert light into energy?",
+        choices: ["Photosynthesis", "Respiration", "Germination"],
+        correctAnswers: "Photosynthesis"
+    },
+    {
+        question: "What is the process by which plants convert light into energy?",
+        choices: ["Photosynthesis", "Respiration", "Germination"],
+        correctAnswers: "Photosynthesis"
+    },
+    {
+        question: "What is the process by which plants convert light into energy?",
+        choices: ["Photosynthesis", "Respiration", "Germination"],
+        correctAnswers: "Photosynthesis"
+    },
+    {
+        question: "What is the process by which plants convert light into energy?",
+        choices: ["Photosynthesis", "Respiration", "Germination"],
+        correctAnswers: "Photosynthesis"
+    },
+    {
+        question: "What is the process by which plants convert light into energy?",
+        choices: ["Photosynthesis", "Respiration", "Germination"],
+        correctAnswers: "Photosynthesis"
+    },
+    
+
+]
 var currentQuestion = [
-    "What is the process by which plants convert light into energy?", 
-    "Which of the following is not a type of plant tissue?", 
-    "Which plant hormone is responsible for promoting cell elongation and growth?", 
+    "What is the process by which plants convert light into energy?",
+    "Which of the following is not a type of plant tissue?",
+    "Which plant hormone is responsible for promoting cell elongation and growth?",
     "Which part of the plant is responsible for absorbing water and minerals from the soil?",
-    "What is the outermost layer of a leaf called?", 
-    "Which of the following is a carnivorous plant?", 
-    "What is the process of pollen transfer from the anther to the stigma called?" 
+    "What is the outermost layer of a leaf called?",
+    "Which of the following is a carnivorous plant?",
+    "What is the process of pollen transfer from the anther to the stigma called?"
 ]
 // arry of answers - varAnswers = ["[4", "5", "6", "7"], ["9", "8", "7", "16"], ["11", "12", "16", "17"], ["24", "25", "26", "27"]]
 var answers = [
-    ['a) Photosynthesis', 'b) Respiration', 'c) Germination'], 
-    ['a) Epidermis', 'b) Xylem', 'c) Mitochondria'], 
-    ['a) Gibberellins', 'b) Ethylene', 'c) Abscisic acid'], 
-    ['a) Roots', 'b) Leaves', 'c) Flowers'], 
-    ['a) Cuticle', 'b) Epidermis', 'c) Mesophyll'], 
-    ['a) Venus flytrap', 'b) Sunflower', 'c) Cactus'], 
+    ['a) Photosynthesis', 'b) Respiration', 'c) Germination'],
+    ['a) Epidermis', 'b) Xylem', 'c) Mitochondria'],
+    ['a) Gibberellins', 'b) Ethylene', 'c) Abscisic acid'],
+    ['a) Roots', 'b) Leaves', 'c) Flowers'],
+    ['a) Cuticle', 'b) Epidermis', 'c) Mesophyll'],
+    ['a) Venus flytrap', 'b) Sunflower', 'c) Cactus'],
     ['a) Pollination', 'b) Fertilization', 'c) Germination']
 ];
 
@@ -78,7 +112,10 @@ var correctAnswers = [
 
 // functions
 
-function startGame () {
+function startGame() {
+    startPage.classList.add("hide");
+    questionsPage.classList.remove("hide");
+    setTime();
     // initalize the game 
     // - start button hides
     // - welcome text disapear 
@@ -87,11 +124,36 @@ function startGame () {
     // - options for question shows
 }
 
+function setTime() {
+    timerInterval = setInterval(function () {
+        if (timeLeft <= 0) {
+            endGame()
+        }
+        timeLeft--;
+        timeEl.textContent = timeLeft;
+
+    }, 1000);
+}
+
+
+
+
+// creat function to stop the game
+function endGame() {
+    clearInterval(timerInterval){
+        // switch to finish page
+    }
+}
+
+// when the submit button is clicked to submit the final score and initals // 
+startButton.addEventListener("click", startGame)
+
+
 // function timer() {
     // set internal interval - look through activites
     // decrement time by time left each second
     // check if the time is zero === endGame
-    // 
+    //
 // }
 
 // function userGuess() {
